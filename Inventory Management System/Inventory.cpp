ecbc8167df98d62199341231b46359d60caea340
YourGi <youremail@gmail.com>
2026-03-24 10:29:04 +0200

    inventory class cpp

---
+++ Inventory Management System/Inventory.cpp
@@ -0,0 +1,36 @@
+#include "Inventory.h"
+#include <iostream>
+#include <string>
+#include<vector>
+#include<fstream>
+#include<sstream>
+using namespace std;
+
+//===================================================           TEAM A
+//     Rana
+void inventory::addproduct(int id, string name, double price, int quantity) {
+
+}
+//     Jana
+void inventory::updatequantity(int id, int quantity) {
+
+}
+//     Habiba
+void inventory::removeproduct(int id) {
+
+}
+//     rawan
+void inventory::displayinventory() {
+
+}
+
+double inventory::computetotalvalue() {
+
+}
+
+
+//--- -------------               Team B
+
+void inventory::lowstockalert(int threshold) {
+
+}
--- Inventory Management System/inventory.h
+++ Inventory Management System/inventory.h
@@ -1,5 +1,43 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include<vector>
+#include<fstream>
+#include<sstream>
+#include"Product.h"
+using namespace std;
 class inventory
 {
+private:
+	 vector <Product> products;
+public:
+	void saveToFile(string IMS) {};
+	void loadToFile(string IMS) {};
+
+	// team A --------------------------------- team A
+
+	      //                   rana
+	void addproduct(int id, string name, double price, int quantity) {
+
+	}
+	      //                   jana
+	void updatequantity(int id, int quantity){
+	}
+         //                    habiba
+	void removeproduct(int id) {
+	}
+       //                      rawan 
+	void displayinventory() {
+	}
+	double computetotalvalue() {
+	}
+
+	// TEAM B ---------------------           ------------------
+	void lowstockalert(int threshold) {
+	}
+
+
+
+
 };
 
