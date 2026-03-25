e560251670684332cf0b101eaf72a7fb36ba39ab
ranaa221 <ana.free0100@gmail.com>
2026-03-25 05:32:09 +0200

    Edited the add new poduct function

--- Inventory Management System/Inventory.cpp
+++ Inventory Management System/Inventory.cpp
@@ -13,10 +13,12 @@ void inventory::addproduct(int id, string name, double price, int quantity) {
 	for (int i = 0; i < products.size(); i++) {
 		if (products[i].getId() == id) {
 			cout << " Error! this product id already exists" << endl ;
+			return;
 		}
 	}
 	// add new product 
-	Product newProduct(int id, string name, double price, int quantity);
+	Product newProduct(id, name, price, quantity);
+	products.push_back(newProduct);
 	cout << " Product added successfully " << endl;
 
 	// display all products ( gad3na meny )
